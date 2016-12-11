94f6bce9c5c904bfa67fd0fda832bab7f7625768
Vladimir Kuprienko <vldmr.kuprienko@gmail.com>
2016-12-11 22:54:24 +0200

    Added unit tests for ConfigLanguageProvider

---
+++ tests/unit/providers/ConfigLanguageProviderTest.php
@@ -0,0 +1,65 @@
+<?php
+namespace bl\emailTemplates\tests\unit\providers;
+
+use bl\emailTemplates\providers\LanguageProviderInterface;
+use bl\emailTemplates\providers\ConfigLanguageProvider;
+
+/**
+ * Test case for ConfigLanguageProvider
+ *
+ * @link https://github.com/black-lamp/yii2-email-templates
+ * @license GNU Public License
+ * @author Vladimir Kuprienko <vldmr.kuprienko@gmail.com>
+ * @copyright Copyright (c) Vladimir Kuprienko
+ */
+class ConfigLanguageProviderTest extends \Codeception\Test\Unit
+{
+    /**
+     * @var \bl\emailTemplates\tests\UnitTester
+     */
+    protected $tester;
+
+    /**
+     * @var \bl\emailTemplates\providers\ConfigLanguageProvider
+     */
+    protected $object;
+
+    /**
+     * @inheritdoc
+     */
+    public function _before()
+    {
+        $this->object = new ConfigLanguageProvider([
+            'languages' => [
+                1 => 'English',
+                2 => 'Russian',
+                3 => 'Ukrainian'
+            ],
+            'defaultLanguage' => [1 => 'English']
+        ]);
+    }
+
+    public function testInstanceOf()
+    {
+        expect('Class must be instance of LanguageProviderInterface', $this->object)
+            ->isInstanceOf(LanguageProviderInterface::class);
+    }
+
+    public function testGetLanguages()
+    {
+        $languages = $this->object->getLanguages();
+        expect('Method should return array', $languages)->internalType('array');
+    }
+
+    public function testGetDefaultLanguage()
+    {
+        $defaultLanguage = $this->object->getDefault();
+        expect('Method should return array', $defaultLanguage)->internalType('array');
+    }
+
+    public function testGetLanguageById()
+    {
+        $language = $this->object->getNameByID(1);
+        expect('Method must return string', $language)->internalType('string');
+    }
+}
